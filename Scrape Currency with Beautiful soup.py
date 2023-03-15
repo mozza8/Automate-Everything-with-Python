e685d9b28ce2e4574d1570b0381d680038275789
mozza8 <zanm.mozoli@almamater.si>
2023-03-15 17:38:24 +0100

    Update Scrape Currency with Beautiful soup.py
    Dodajanje komentarjev

--- Scrape Currency with Beautiful soup.py
+++ Scrape Currency with Beautiful soup.py
@@ -1,12 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
+# Selenium se uporablja za avtomatizacijo v brskalnikih(klikanje, izpolnjevanje, login,..), medtem ko se beautiful soup uporablja za scrapanje podatkov iz brskalnika
+# Ne rabiš odpreti brskalnika
 
 def get_currency(in_currency, out_currency):
     url = f'https://www.x-rates.com/calculator/?from={in_currency}&to={out_currency}&amount=1'
-    content = requests.get(url).text
-    soup = BeautifulSoup(content, 'html.parser')
-    rate = soup.find("span", class_="ccOutputRslt").get_text()
+    content = requests.get(url).text                  # content je html stran v tekstovni oblik
+    soup = BeautifulSoup(content, 'html.parser')      # parse = razčleniti, BeautifulSoup pozna strukturo html-a
+    rate = soup.find("span", class_="ccOutputRslt").get_text()  # span element, določen class
     rate = float(rate[:-4])
 
     return rate
